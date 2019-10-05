02a71e7f6ab9d8a41176ef56ebeccbacd594c213
Peter Sarkis <petersarkis@gmail.com>
2019-10-05 14:47:02 -0500

    added starting css, ts and updated html file

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-console.log("test");
+let canvas:HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("game");
+let ctx = canvas.getContext("2d");
+let aspectRatio = 16 / 10;
+let windowedWidth = 1024;
+let windowedHeight = 640;
+canvas.width = windowedWidth;
+canvas.height = windowedHeight;
+
+function update() {
+  requestAnimationFrame(update);
+}
+
+
+window.addEventListener("load", function () {
+  update();
+});
